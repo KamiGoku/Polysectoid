9ea19faa210c07fc60425788d8de36252fb3ad1c
ashkanvafaee <ashkanvafaee@utexas.edu>
2018-10-22 13:11:09 -0500

    Transmission Receiver Code

---
+++ Transmission/Receiver/Receiver.ino
@@ -0,0 +1,28 @@
+#include <Pattern.h>
+#include <BasicLinearAlgebra.h>
+#include <SoftwareSerial.h>
+#include <math.h>
+
+void setup() {
+  Serial.begin(9600);
+
+}
+
+void loop() {
+  
+  // Read 5 bytes at a time
+  if(Serial.available() >= 5){
+    char rec_packet[5];
+    Serial.readBytes(rec_packet, 5);
+    rec_packet[4] = '\n';
+
+    // Checking if data was meant for this arduino
+    if(rec_packet[0] == '0'){
+      // Perform some useful work
+      Serial.write(rec_packet,5);           // Display the received value 
+    }
+    
+  }
+
+  delay(100);
+}
