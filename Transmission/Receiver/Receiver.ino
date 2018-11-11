12eead86f39fe6bb90f7dfb3541c43ddb7198ee0
ashkanvafaee <ashkanvafaee@utexas.edu>
2018-11-11 00:12:02 -0600

    Sender & Receiver code updated with RingBuf

--- Transmission/Receiver/Receiver.ino
+++ Transmission/Receiver/Receiver.ino
@@ -2,28 +2,53 @@
 #include <BasicLinearAlgebra.h>
 #include <SoftwareSerial.h>
 #include <math.h>
+#include <RingBuf.h>
+
+
+RingBuf *buf = RingBuf_new(5 * sizeof(char), 10); // Buffer that holds 10 packets
 
 void setup() {
   Serial.begin(9600);
 
+  cli(); //disable interrupts 
+  
+  //Setup Timer 0 to trigger every 66 Hz
+  TCCR0A = 0;// set entire TCCR0A register to 0
+  TCCR0B = 0;// same for TCCR0B
+  TCNT0  = 0;//initialize counter value to 0
+  // set compare match register for 2khz increments
+  OCR0A = 236;// = (16*10^6) / (66*1024) - 1 (must be <256)     236
+  // turn on CTC mode
+  TCCR0A |= (1 << WGM01);
+  // Set CS01 and CS00 bits for 1024 prescaler
+  TCCR0B |= (1 << CS02) | (1 << CS00);   
+  // enable timer compare interrupt
+  TIMSK0 |= (1 << OCIE0A);
+
+  sei(); //enable interrupts
+
 }
 
-void loop() {
-  
-  // Read 5 bytes at a time
+// This ISR will receive packets sent from
+// another arduino and put them in the buffer
+ISR (TIMER0_COMPA_vect){
+  // Read a packet if there is one
   if(Serial.available() >= 5){
-    char rec_packet[5];
-    Serial.readBytes(rec_packet, 5);
-    //rec_packet[4] = '\n';
-
-    // Checking if data was meant for this arduino
-    //if(rec_packet[0] == '0'){
-      // Perform some useful work
-      Serial.write(rec_packet,5);           // Display the received value 
-      //Serial.write('\n');
-    //}
-    
+    char packet[5];
+    Serial.readBytes(packet,5);
+    if(!buf->isFull){
+      buf->add(buf, &packet);
+    }
   }
 
-  delay(1);
+}
+
+// In here we are just outputting packets from the buffer
+void loop() {
+  if(!buf->isEmpty){
+    char packet[5];
+    buf->pull(buf, &packet);
+    Serial.write(packet, 5);
+  }
+
 }
--- Transmission/Sender/Sender.ino
+++ Transmission/Sender/Sender.ino
@@ -2,61 +2,82 @@
 #include <BasicLinearAlgebra.h>
 #include <SoftwareSerial.h>
 #include <math.h>
+#include <RingBuf.h>
 
 
-int sensorPin = A0;   // Select the input pin for the potentiometer
-
+int sensorPin1 = A0;                              // One of the sensors
+int sensorPin2 = A1;                              // The other sensor in the same segment   
+RingBuf *buf = RingBuf_new(5 * sizeof(char), 10); // Buffer that holds 10 packets
 
 void setup() {
-  pinMode(sensorPin,INPUT);
+  pinMode(sensorPin1,INPUT);
+  pinMode(sensorPin2,INPUT);
   Serial.begin(9600);
+
+  cli(); //disable interrupts 
+  
+  // Setup Timer 1 to trigger every 30 Hz
+  TCCR1A = 0;// set entire TCCR1A register to 0
+  TCCR1B = 0;// same for TCCR1B
+  TCNT1  = 0;//initialize counter value to 0
+  // set compare match register for 1hz increments
+  OCR1A = 520;// = (16*10^6) / (30*1024) - 1 (must be <65536)  520
+  // turn on CTC mode
+  TCCR1B |= (1 << WGM12);
+  // Set CS10 and CS12 bits for 1024 prescaler
+  TCCR1B |= (1 << CS12) || (1 << CS10);  
+  // enable timer compare interrupt
+  TIMSK1 |= (1 << OCIE1A);
+
+  sei(); //enable interrupts
 }
 
+// This ISR will send the packets stored in the
+// buffer to another arduino via the TX port
+ISR(TIMER1_COMPA_vect){
+  if(!buf->isEmpty){
+    char packet[5];
+    buf->pull(buf,&packet);
+    Serial.write(packet, 5);
+  }
+}
+
+
+// In here we are just filling up the buffer with packets
 void loop() {
 
-  // Sending Data
-  char send_packet [5];
-  int adcVal = analogRead(sensorPin);
-  send_packet[4] = '\n';
-  send_packet[3] = (adcVal % 10)+0x30;
-  adcVal = adcVal/10;
-  send_packet[2] = (adcVal % 10)+0x30;
-  adcVal = adcVal/10;
-  send_packet[1] = (adcVal % 10)+0x30;
-  adcVal = adcVal/10;
-  send_packet[0] = (adcVal % 10) + 0x30;
+  int s_val1 = analogRead(sensorPin1);
+  int s_val2 = analogRead(sensorPin2);
 
-  // Formatting Data
-  /*if(adcVal < 10){
-    send_packet[4] = send_packet[0];
-    send_packet[3] = '0';
-    send_packet[2] = '0';
-    send_packet[1] = '0';
-  }
-  else if (adcVal < 100){
-    send_packet[4] = send_packet[1];
-    send_packet[3] = send_packet[0];
-    send_packet[2] = '0';
-    send_packet[1] = '0';
-  }
-  else if(adcVal < 1000){
-    send_packet[4] = send_packet[2];
-    send_packet[3] = send_packet[1];
-    send_packet[2] = send_packet[0];
-    send_packet[1] = '0';
-  }
-  else {
-    send_packet[4] = send_packet[3];
-    send_packet[3] = send_packet[2];
-    send_packet[2] = send_packet[1];
-    send_packet[1] = send_packet[0];
-  }*/
-  
-  //send_packet[0] = '0';
-  Serial.write(send_packet,5); 
-  //Serial.println(adcVal);
+  // Calibrating left sensor to 0-500
+  if (s_val1 <= 0) s_val1 = 0;
+  else if (s_val1 > 605) s_val1 = 605;
+  s_val1 = ((s_val1 - 0)*500)/605;
+  s_val1 = 500 - s_val1;//flip
 
-  delay(10);
+
+  // Calibrating right sensor to 0-500
+  if (s_val2 < 30) s_val2 = 30;
+  else if (s_val2 > 450) s_val2 = 450;
+  s_val2 = ((s_val2 - 30)*500)/420;
+
+  // Average left and right vals
+  int adcVal = (s_val1 + s_val2) / 2;
+
+  // Converting data into a packet and storing it in buffer
+  char packet [5];
+  packet[4] = '\n';
+  packet[3] = (adcVal % 10) + 0x30;
+  adcVal = adcVal/10;
+  packet[2] = (adcVal % 10) + 0x30;
+  adcVal = adcVal/10;
+  packet[1] = (adcVal % 10) + 0x30;
+  adcVal = adcVal/10;
+  packet[0] = (adcVal % 10) + 0x30;
+
+  if(!buf->isFull(buf)){
+    buf->add(buf,&packet);
+  }
   
 }
 
