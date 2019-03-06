7c711517619f6f8dabab9efae8848b4a075bfdcf
ashkanvafaee <ashkanvafaee@utexas.edu>
2019-03-06 14:05:54 -0600

    Revert "temp committ"
    This reverts commit 432d7de813898c7cd89333553ed3e2f04e573625.

--- CPG_Main2/brain.cpp
+++ CPG_Main2/brain.cpp
@@ -6,33 +6,34 @@
 
 class Brain {
 public:
-  float weights[NUM * 3][SIZE];   // Weights for neighboring arduinos
-  float int_freq [NUM * 3];       // Intrinsic Frequency
-  float bias[NUM * 3][SIZE];      // Bias for neighboring arduinos
-  float tau;                      // Frequency parameter
+	float weights[NUM * 3][SIZE];		// Weights for neighboring arduinos
+	float int_freq [NUM * 3];				// Intrinsic Frequency
+	float bias[NUM * 3][SIZE];			// Bias for neighboring arduinos
+	float tau;								      // Frequency parameter
 
-  Brain(Pattern pattern, float Tau) {
-    tau = Tau;
-    
-    //Weight calculation
-    for (int k = 0; k < 3; k++) {
-      float weight_row[NUM], bias_row[NUM];
-      /*weight_row[0] = pattern.weight_pat_after[k][0];
-      weight_row[1] = pattern.weight_pat_after[k][1];
-      weight_row[2] = 0;
-      weight_row[3] = pattern.weight_pat_before[k][0];
-      weight_row[4] = pattern.weight_pat_before[k][1];
-      bias_row[0] = -1*pattern.bias_pat_after[k][0] - pattern.bias_pat_after[k][1];
-      bias_row[1] = -1*pattern.bias_pat_after[k][0];
-      bias_row[2] = 0;
-      bias_row[3] = pattern.bias_pat_before[k][0];
-      bias_row[4] = pattern.bias_pat_before[k][0] + pattern.bias_pat_before[k][1];*/
-      
+	Brain(Pattern pattern, float Tau) {
+		tau = Tau;
+		
+		//Weight calculation
+		for (int k = 0; k < 3; k++) {
+			float weight_row[NUM], bias_row[NUM];
+			/*weight_row[0] = pattern.weight_pat_after[k][0];
+			weight_row[1] = pattern.weight_pat_after[k][1];
+			weight_row[2] = 0;
+			weight_row[3] = pattern.weight_pat_before[k][0];
+			weight_row[4] = pattern.weight_pat_before[k][1];
+
+			bias_row[0] = -1*pattern.bias_pat_after[k][0] - pattern.bias_pat_after[k][1];
+			bias_row[1] = -1*pattern.bias_pat_after[k][0];
+			bias_row[2] = 0;
+			bias_row[3] = pattern.bias_pat_before[k][0];
+			bias_row[4] = pattern.bias_pat_before[k][0] + pattern.bias_pat_before[k][1];*/
+			
 
 
-      for (int i = k*5; i < (k+1)*5; i++) {
-        weights[i][5] = pattern.cross_weight[k][0];
-        weights[i][6] = pattern.cross_weight[k][1];
+			for (int i = k*5; i < (k+1)*5; i++) {
+				weights[i][5] = pattern.cross_weight[k][0];
+				weights[i][6] = pattern.cross_weight[k][1];
         bias[i][5] = 0;
         bias[i][6] = 0;
 
@@ -50,35 +51,35 @@ public:
           }
         }
 
-        /*for (int j = 0; j < 5; j++) {
-          if (i == 0 && j < 3) {
-            weights[i][j] = weight_row[j + 2];
+				/*for (int j = 0; j < 5; j++) {
+					if (i == 0 && j < 3) {
+						weights[i][j] = weight_row[j + 2];
             bias[i][j] = bias_row[j+2];
-          }
-          else if (i == 1 && j < 4) {
-            weights[i][j] = weight_row[j + 1];
+					}
+					else if (i == 1 && j < 4) {
+						weights[i][j] = weight_row[j + 1];
             bias[i][j] = bias_row[j+1];
-          }
-          else if (i == 2 && j < 5) {
-            weights[i][j] = weight_row[j];
+					}
+					else if (i == 2 && j < 5) {
+						weights[i][j] = weight_row[j];
             bias[i][j] = bias_row[j];
-          }
-          else if (i == 3 && j > 0) {
-            weights[i][j] = weight_row[j - 1];
+					}
+					else if (i == 3 && j > 0) {
+						weights[i][j] = weight_row[j - 1];
             bias[i][j] = bias_row[j-1];
-          }
-          else if (i == 4 && j > 1) {
-            weights[i][j] = weight_row[j - 2];
+					}
+					else if (i == 4 && j > 1) {
+						weights[i][j] = weight_row[j - 2];
             bias[i][j] = bias_row[j-2];
-          }
-        }*/
-      }
-    }
+					}
+				}*/
+			}
+		}
 
-    for (int i = 0; i < 15; i++) {
-      int_freq[i] = pattern.int_freq[i];
-    }
+		for (int i = 0; i < 15; i++) {
+			int_freq[i] = pattern.int_freq[i];
+		}
 
-  }
+	}
  
 };
