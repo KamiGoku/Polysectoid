b812398a11060e0e5f49b77550397e3ebfdf6b3a
Ashkan Vafaee <ashkanvafaee@utexas.edu>
2019-04-07 03:54:43 -0500

    Individual Segment Code Added

--- Integration Testing/CPG + Serial (5 Segments + Brain)/Segment1/Segment1.ino
+++ Integration Testing/CPG + Serial (5 Segments + Brain)/Segment1/Segment1.ino
@@ -1,6 +1,6 @@
 #include "cpg.cpp"  
 
- Actuator actuators [3];    // Oscillators 0, 5, 10
+Actuator actuators [3];    // Oscillators 0, 5, 10
 
 void setup() {
   Serial.begin(9600);
@@ -10,7 +10,7 @@ void setup() {
   actuators[1].phase = 0.42;
   actuators[2].phase = 0.73;
 
-  // IMPORT ALL THE ABOVE VARIABLES HERE FROM BRAIN RAIYAN :)
+  // IMPORT ALL FIELDS FOR EACH ACTUATOR HERE
   while(1){           // Break out once all 3 actuatos have received all data from brain
 
 
@@ -18,19 +18,40 @@ void setup() {
   }
 
   delay(1000);      // Chill for a sec to make sure all other arduinos are ready
-
 }
 
 void loop() {
     // Step 1: Each arduino sends their phase to 7 other arduinos
     for(int i = 0; i < 3; i++){
+      if(i == 0){
+        actuators[0].neighbor_phases[0] = actuators[i].phase;
+        actuators[1].neighbor_phases[5] = actuators[i].phase;
+        actuators[2].neighbor_phases[5] = actuators[i].phase;
+        sendData(0,actuators[i].phase);
+      }
+      else if(i == 1){
+        actuators[0].neighbor_phases[5] = actuators[i].phase;
+        actuators[1].neighbor_phases[0] = actuators[i].phase;
+        actuators[2].neighbor_phases[6] = actuators[i].phase;
+        sendData(5,actuators[i].phase);        
+      }
+      else{
+        actuators[0].neighbor_phases[6] = actuators[i].phase;
+        actuators[1].neighbor_phases[6] = actuators[i].phase;
+        actuators[2].neighbor_phases[0] = actuators[i].phase;
+        sendData(10,actuators[i].phase);          
+      }
       
     }
 
-    // Step 2: wait until each arduino has gotten phase data from the other 7 arduinos
+    // Step 2: Wait until each arduino has gotten phase data from the other 7 arduinos
     for(int i = 0; i < 3; i++){
       bool ready = false;
       while(!ready){
+        // Read phase data here
+        receiveData();
+
+        
         ready = true;
         for(int j = 0; j < NUM; j++){
           if(actuators[i].neighbor_phases[j] == 0){ready = false;};
@@ -38,17 +59,24 @@ void loop() {
       }
     }
     
-    
 
-    //Serial.print( (float) phase );  
+    // Step 3: Update the phases of each arduino
+    for(int i = 0; i < 3; i++){
+      actuators[i].update_phase();
+    }
 
-  
+    //Serial.print( (float) phase );    
 }
 
 // Send phase data to oscillator index
 void sendData(int index, float phase){
 
 
+  
+}
+
+// Receieve phase data from other oscillators
+void receiveData(){
   
 }
 
--- Integration Testing/CPG + Serial (5 Segments + Brain)/Segment1/cpg.cpp
+++ Integration Testing/CPG + Serial (5 Segments + Brain)/Segment1/cpg.cpp
@@ -8,36 +8,38 @@ public:
 
 	float weights[SIZE];		// Weights for neighboring arduinos (0-4 -> vertical connection) (5,6 -> horizontal connections)
 	float int_freq;			    // Intrinsic Frequency
-	float bias[SIZE];			  // Bias for neighboring arduinos
+	float biases[SIZE];			// Bias for neighboring arduinos
 	float tau;					    // Frequency parameter
 	float phase;				    // Current phase of this actuator 
   int index;              // ID number of Oscillator
 
   float neighbor_phases[SIZE] = {0}; // Phases of neighboring arduinos
 
- Actuator(){}
+  Actuator(){}
 
-	float update_phase(float neighbor_phases[]) {
-		float sum = 0.0;
+	float update_phase() {
+    float sum = 0.0;
 
-		for (int i = 0; i < SIZE; i++) {
-			//sum += (weights[i] * sin((double) ((neighbor_phases[i] - phase) * 2 * M_PI - bias[i])));
-      double intpart,fracpart;
-      fracpart = modf(neighbor_phases[i], &intpart);
-      intpart = intpart/10000;
+    for (int i = 0; i < SIZE; i++) {
+      double weight,bias;
+      bias = modf(biases[i], &weight);
+      weight = weight/10000.0;
 
-      if(index == 0 || index == 1 || index == 5 || index == 6 || index == 10 || index == 11){
-        fracpart *= -1;
+      if(((index==1 || index==6 || index==11) && (i==0)) || 
+         ((index==2 || index==7 || index==12) && (i==0 || i==1)) ||
+         ((index==3 || index==8 || index==13) && (i==1 || i==2)) ||
+         ((index==4 || index==9 || index==14) && (i==2 || i==3))){
+          
+          bias *= -1;
       }
-
-      sum += (intpart * sin((double) ((neighbor_phases[i] - phase) * 2 * M_PI - fracpart)));
-		}
+      sum += weight * sin((double) ((neighbor_phases[i] - phase) * 2 * M_PI - bias));
+    }
 
     sum = ((2 * M_PI * int_freq) + sum) / (2 * M_PI * tau);
 
-		double modVal = 1;
-		phase = modf(sum + phase, &modVal);
+    double modVal = 1;
+    phase = modf(sum + phase, &modVal);
 
-		return (float) phase;
+    return (float) phase;
 	}
 };
