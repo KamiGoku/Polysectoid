df99e5ca6f4025e802d24b9b2ba9787d81c043e1
Ashkan Vafaee <ashkanvafaee@utexas.edu>
2019-04-12 15:32:38 -0500

    Integration 3 Init

---
+++ Integration Testing 3/Brain/brain.cpp
@@ -0,0 +1,89 @@
+#define numBefore 2
+#define numAfter 2
+#define NUM 5
+#define SIZE NUM + 2
+
+// Brain Variables  
+const float bias_pat_before[3][numBefore] = {{0.1, 0.1},
+                                             {0.1, 0.1},
+                                             {0.1, 0.1}};
+const float bias_pat_after[3][numAfter] = {{0.1, 0.1},
+                                           {0.1, 0.1},
+                                           {0.1, 0.1}};
+
+const float weight_pat_before [3][numBefore] = {{0.008, 0.004},
+                                                {0.02, 0.01},
+                                                {0.008, 0.004}};
+const float weight_pat_after [3][numAfter] = {{0.008, 0.004},
+                                              {0.02, 0.01},
+                                              {0.008, 0.004}};
+
+const float cross_weight [3][2] = {{0.0003, 0.0003},
+                                   {0.0003, 0.0003},
+                                   {0.0003, 0.0003}};
+                             
+const float cross_bias [3][2] = {{0, 0},
+                                         {0, 0},
+                                         {0, 0}};
+
+const float int_freq[15] = {0.02, 0.02, 0.02, 0.02, 0.02, 0.01, 0.01, 0.01, 0.01, 0.01, 0.02, 0.02, 0.02, 0.02, 0.02};
+
+const float init_phases [] = {.1, .7, .8, .23, .6, .42, .7, .9, .8, .6, .73, .7, .5, .44, .6};
+
+const float tau = 1.0;                                // Frequency parameter
+
+
+class Brain {
+public:
+	float weights_bias[NUM*3][SIZE] = {{0}};	      // Integer part = weight, Fractional part = bias
+
+	Brain() {
+    for (int k = 0; k < 3; k++) {
+      float weight_row[NUM];
+      float bias_row[NUM];
+      
+      bias_row[0] = bias_pat_after[k][0] + bias_pat_after[k][1];  // Assume negative
+      bias_row[1] = bias_pat_after[k][0];                         // Assume negative
+      bias_row[2] = 0;
+      bias_row[3] = bias_pat_before[k][0];
+      bias_row[4] = bias_pat_before[k][0] + bias_pat_before[k][1];
+      
+      weight_row[0] = weight_pat_after[k][1];
+      weight_row[1] = weight_pat_after[k][0];
+      weight_row[2] = 0;
+      weight_row[3] = weight_pat_before[k][0];
+      weight_row[4] = weight_pat_before[k][1];  
+
+      for (int i = k*NUM; i < (k+1)*NUM; i++) {
+        weights_bias[i][5] += cross_weight[k][0]*10000;
+        weights_bias[i][6] += cross_weight[k][1]*10000;
+        weights_bias[i][5] += cross_bias[k][0];
+        weights_bias[i][6] += cross_bias[k][1];
+        
+        for (int j = 0; j < 5; j++) {
+          if ((i == k*5) && j < 3) {
+            weights_bias[i][j] += weight_row[j + 2]*10000;
+            weights_bias[i][j] += bias_row[j + 2];
+          }
+          else if ((i == (k*5)+1) && j < 4) {
+            weights_bias[i][j] += weight_row[j + 1]*10000;
+            weights_bias[i][j] += bias_row[j + 1];
+          }
+          else if ((i == (k*5)+2) && j < 5) {
+            weights_bias[i][j] += weight_row[j]*10000;
+            weights_bias[i][j] += bias_row[j];
+          }
+          else if ((i == (k*5)+3) && j > 0) {
+            weights_bias[i][j] += weight_row[j - 1]*10000;
+            weights_bias[i][j] += bias_row[j - 1];
+          }
+          else if ((i == (k*5)+4) && j > 1) {
+            weights_bias[i][j] += weight_row[j - 2]*10000;
+            weights_bias[i][j] += bias_row[j - 2];
+          }
+        }
+      }
+    }    
+	} 
+};
+
---
+++ Integration Testing 3/Segment1/Segment1.ino
@@ -0,0 +1,66 @@
+#include "communication.cpp"
+
+Actuator actuators [3];    // Oscillators 0, 5, 10
+
+void setup() {
+  Serial.begin(9600);
+
+  // Hardcoding initial phases
+  actuators[0].phase = 0.1;
+  actuators[1].phase = 0.42;
+  actuators[2].phase = 0.73;
+
+  // Importing brain data
+  getBrainData();          // Breaks out once all 3 actuators have received all data from brain
+
+  delay(1000);      // Chill for a sec to make sure all other arduinos are ready
+}
+
+void loop() {
+    // Step 1: Each arduino sends their phase to 7 other arduinos
+    for(int i = 0; i < 3; i++){
+      if(i == 0){
+        actuators[0].neighbor_phases[0] = actuators[i].phase;
+        actuators[1].neighbor_phases[5] = actuators[i].phase;
+        actuators[2].neighbor_phases[5] = actuators[i].phase;
+        //sendData(0,actuators[i].phase);
+      }
+      else if(i == 1){
+        actuators[0].neighbor_phases[5] = actuators[i].phase;
+        actuators[1].neighbor_phases[0] = actuators[i].phase;
+        actuators[2].neighbor_phases[6] = actuators[i].phase;
+        //sendData(5,actuators[i].phase);        
+      }
+      else{
+        actuators[0].neighbor_phases[6] = actuators[i].phase;
+        actuators[1].neighbor_phases[6] = actuators[i].phase;
+        actuators[2].neighbor_phases[0] = actuators[i].phase;
+        //sendData(10,actuators[i].phase);          
+      }
+      
+    }
+
+    // Step 2: Wait until each arduino has gotten phase data from the other 7 arduinos
+    for(int i = 0; i < 3; i++){
+      bool ready = false;
+      while(!ready){
+        // Read phase data here
+        //receiveData();
+
+        
+        ready = true;
+        for(int j = 0; j < NUM; j++){
+          if(actuators[i].neighbor_phases[j] == 0){ready = false;};
+        }
+      }
+    }
+    
+
+    // Step 3: Update the phases of each arduino
+    for(int i = 0; i < 3; i++){
+      actuators[i].update_phase();
+    }
+
+    //Serial.print( (float) phase );    
+}
+
